931c12765c853e6b380cddfb63e46c63836fc2cc
SRO2k20 <sjfrouilly@gmail.com>
2020-07-20 20:49:52 -0400

    Background objects

---
+++ Assets/_Project/Scenes/BackgroundAudioSource.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudioSource : MonoBehaviour
+{
+
+    private AudioSource theAudioSource;
+
+    public AudioClip backgroundConstructionSite;
+
+
+    void Awake()
+    {
+        theAudioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        PlayBackground();
+    }
+
+    public void PlayBackground()
+    {
+        theAudioSource.loop = true;
+        theAudioSource.volume = 0.02f;
+        theAudioSource.clip = backgroundConstructionSite;
+        theAudioSource.Play();
+    }
+
+}
