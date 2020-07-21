07c5eae3c4da2679674e9fe605592820348000ab
Rahul Arasikere <arasikere.rahul@gmail.com>
2020-07-22 04:52:08 +0530

    some more work done

--- Assets/_Project/Scripts/UIManager.cs
+++ Assets/_Project/Scripts/UIManager.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class UIManager : Singleton<UIManager>
 {
     // Start is called before the first frame update
-    void Start()
+    public GameObject ignition;
+    public GameObject rotateCrane;
+    public GameObject rotateBoom;
+    public GameObject extendPulley;
+    public GameObject extendBoom;
+    public GameObject pickUp;
+    public GameObject dropBoom;
+
+    public void IgnitionDone()
     {
-        
+        ignition.transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void RotateCraneDone()
+    {
+        rotateCrane.transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void RotateBoomDone()
+    {
+        rotateBoom.transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void ExtendPulleyDone()
+    {
+        extendPulley.transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void ExtendBoomDone()
+    {
+        extendBoom.transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void PickUpDone()
+    {
+        pickUp.transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void DropBoomDone()
+    {
+        dropBoom.transform.GetChild(0).gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
